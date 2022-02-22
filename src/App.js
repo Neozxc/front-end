b162a5c8bb49278c39b67df78d71ae9aa7c3f55c
Nedas <neduska991@gmail.com>
2022-02-22 12:01:32 +0000

    Changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { useState } from "react";
+import { fetchRequest } from './utils';
 
-function App() {
+const App = () => {
+
+  const [user, setUser] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [username, setUsername] = useState();
+
+  const submitHandler = (e) => {
+    // Stop from re-rendering.
+    e.preventDefault();
+    fetchRequest(setUser, username, email, password);
+  };
+
   return (
-    <div className="App">
-      <h1>Hey</h1>
+    <div>
+      <h1>{user ? `Welcome ${user}` : `Please sign up`}</h1>
+      <form onSubmit={submitHandler}>
+        <input onChange={(e) => setUsername(e.target.value)} />
+        <input onChange={(e) => setEmail(e.target.value)} />
+        <input onChange={(e) => setPassword(e.target.value)} />
+  {/* Because our button is now in form we change type to submit */}
+        <button type='submit'>Sign up</button>
+      </form>
     </div>
   );
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,15 @@
-export const fetchRequest = async () => {
+export const fetchRequest = async (setUser, username, email, password) => {
     try {
-        const response = await fetch("http://localhost:3200/user", {
+        const response = await fetch("http://localhost:3001/user", {
             method: "POST",
+            mode: "no-cors",
             headers: { "Content-Type" : "application/json" },
             body: JSON.stringify({
-                username: "Nedas",
-                email: "nedas@gmail.com",
-                password: ""
+                username: username,
+                email: email,
+                password: password
             })
         });
-
         const data = await response.json();
         console.log(data);
     } catch (error) {
